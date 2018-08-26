fce9f50dc41bed70336b919696539bc3b01c4479
Marc-Alexandre Blanchard <marc.alexandre.blanchard@gmail.com>
2018-08-26 22:07:33 +0200

    Replace carrousel by Flipview in order to ease touch interaction

---
+++ TrueTwitter/Utils/CountToVisibilityConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace TrueTwitter.Utils
+{
+    /// <summary>
+    /// Convert boolean to visibility
+    /// 
+    /// true -> visible
+    /// false -> collapsed
+    /// </summary>
+    public class CountToVisibilityConverter : IValueConverter
+    {
+        public CountToVisibilityConverter()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                var count = int.Parse(value.ToString());
+                if (count > 0)
+                {
+                    return Visibility.Visible;
+                }
+            }
+            catch
+            {
+
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
